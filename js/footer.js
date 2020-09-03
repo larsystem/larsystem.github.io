d5f34561c0505ac62936b10f0f2c9f4868323114
larsystem <65312075+larsystem@users.noreply.github.com>
2020-09-03 22:39:26 +0100

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,21 @@
+document.write(
+'<footer>'+
+'<div class="container">'+
+    '<div class="row">'+
+        '<div class="col-lg-10 col-lg-offset-1 text-center">'+
+            '<div class="spacer-quarter"></div>'+
+            '<h4 style="font-family: Oxygen; margin-bottom: 10px;">Luke Waddington Personal Project</h4>'+
+            '<ul class="list-unstyled" style="font-family: Oxygen;">'+
+                '<li>'+
+                    'Twitter: <a href="https://twitter.com/lukesterwad" target="_blank">LukesterWad</a>'+
+                '</li>'+
+                '<li>'+
+                    'E-Mail: <a href="mailto:lukesterwad@gmail.com?subject=From Website:" target="_blank">lukesterwad@gmail.com</a>'+
+                '</li>'+
+            '</ul>'+
+            '<div class="spacer-half"></div>'+
+        '</div>'+
+    '</div>'+
+'</div>'+
+'</footer>'
+);
---
+++ js/header.js
@@ -0,0 +1,9 @@
+document.write(
+    '<nav id="main">'+
+        '<ul class="nav-links">'+
+            '<li><a href="../">Home</a></li>'+
+            '<li><a href="../contact.html">Contact</a></li>'+
+            '<li><a href="#">Projects</a></li>'+
+        '</ul>'+
+    '</nav>'
+);
