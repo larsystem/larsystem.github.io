8b7160597f618ec17f0324f6ef3a2e518392d0f3
larsystem <65312075+larsystem@users.noreply.github.com>
2020-12-19 14:12:48 +0000

    Delete header.js

--- js/header.js
+++
@@ -1,9 +0,0 @@
-document.write(
-    '<nav id="main">'+
-        '<ul class="nav-links">'+
-            '<li><a href="../">Home</a></li>'+
-            '<li><a href="../contact.html">Contact</a></li>'+
-            '<li><a href="#">Projects</a></li>'+
-        '</ul>'+
-    '</nav>'
-);
